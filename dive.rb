76e039a707892a968fe9e490ee51352e59e1791a
Lyle Johnson <lyle.johnson@synapsewireless.com>
2021-12-02 15:22:51 -0600

    day 2

---
+++ dive.rb
@@ -0,0 +1,38 @@
+Command = Struct.new(:command, :count)
+
+class Submarine
+  attr_reader :horizontal_position
+  attr_reader :depth
+  
+  def initialize
+    @horizontal_position = 0
+    @depth = 0
+  end
+  
+  def accept(command)
+    case command.command
+    when :forward
+      @horizontal_position += command.count
+    when :up
+      @depth -= command.count
+    when :down
+      @depth += command.count
+    end
+  end
+end
+
+def get_commands(filename)
+  IO.readlines(filename).map {|s|
+    tokens = s.split()
+    Command.new(tokens[0].to_sym, tokens[1].to_i)
+  }
+end
+
+if __FILE__ == $0
+  commands = get_commands("dive_commands.txt")
+  sub = Submarine.new
+  commands.each {|command| sub.accept(command) }
+  puts "Horizontal Position: #{sub.horizontal_position}"
+  puts "Depth: #{sub.depth}"
+  puts "Answer: #{sub.horizontal_position * sub.depth}"
+end
